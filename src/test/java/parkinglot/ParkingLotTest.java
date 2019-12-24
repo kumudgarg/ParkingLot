a442a94f7987885bdf96fe9066121152d37c0b95
kumudgarg <kumud50garg@gmail.com>
2019-12-24 18:24:38 +0530

    Added : test case to get the same car which is parked

--- src/test/java/parkinglot/ParkingLotTest.java
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -9,4 +9,10 @@ public class ParkingLotTest {
             Drivers drivers = new Drivers("Allen");
             Assert.assertEquals("Allen", drivers.getName());
         }
+
+    @Test
+    public void givenParkedCar_ShouldBeUnParkedTheSame() {
+        Car car = new Car("Innova");
+        Assert.assertEquals("Innova",car.getCarName());
+    }
 }
