90f2f8193b2c76940b3c16457103a2c118df6f18
kumudgarg <kumud50garg@gmail.com>
2019-12-24 18:11:49 +0530

    Added : test to get return same driver which is allotted

--- src/test/java/parkinglot/Drivers.java
+++ src/test/java/parkinglot/Drivers.java
@@ -1,4 +1,13 @@
 package parkinglot;
 
 public class Drivers {
+    private String driverName;
+
+    public Drivers(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getName() {
+        return driverName;
+    }
 }
--- src/test/java/parkinglot/ParkingLotTest.java
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -1,4 +1,12 @@
 package parkinglot;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingLotTest {
+        @Test
+        public void givenAllottedDriver_ShouldReturnSameDriver() {
+            Drivers drivers = new Drivers("Allen");
+            Assert.assertEquals("Allen", drivers.getName());
+        }
 }
